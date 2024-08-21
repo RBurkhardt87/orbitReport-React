ddc476e9bd1ce3d9ade7b10d300fb9750fdf7b05
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-08-21 01:58:15 -0500

    Checking on if push is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import {useState} from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+
+export default function Banner () {
+    return (
+        <>
+            <header> Orbit Report </header>
+            <p> In order to see the satellites in their orbit type, please click the buttons </p>
+        </>
+    );
+};
+
