48947793f6a5b3887fcc25ac3fc7068be029e487
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-08-24 13:51:38 -0500

    Passed all the test. No Styling applied

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,28 @@ import satData from "./components/satData";
 
 
 function App() {
+  
   const [sat, setSat] = useState(satData);
-  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  
+  const orbitTypes = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
 
   return (
-    <div>
+    <>
       <Banner />
-      <Buttons />
-      <Table />
-    </div>
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        orbitTypes={orbitTypes}
+      />
+      <Table sat={sat} />
+  </>
   );
 }
 
